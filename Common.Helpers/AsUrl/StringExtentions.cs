0ca41cfbce9a004ad77b233130e20148e0d6f53c
Scott Williams <tocsoft@gmail.com>
2012-03-19 11:00:29 +0000

    fixed bug

--- Common.Helpers/AsUrl/StringExtentions.cs
+++ Common.Helpers/AsUrl/StringExtentions.cs
@@ -8,20 +8,17 @@ namespace Common.Helpers.AsUrl
     public static class StringExtentions
     {
         public static string AddQueryString(this string str, params object[] values) {
-
             char sep = '?';
             if (str.Contains(sep)) { sep = '&'; }
             StringBuilder sb = new StringBuilder(str);
             foreach(var dict in values.Select(x=>x.ToDictionary())){
-
                 foreach (var kvp in dict)
                 {
-
-                    sb.Append(kvp.Key);
                     sb.Append(sep);
+                    sb.Append(kvp.Key);
+                    sb.Append("=");
                     sb.Append(kvp.Value.ToString().UrlEncode());
                     sep= '&';
-
                 }
             }
 
