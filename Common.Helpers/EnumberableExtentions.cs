fbfa1a107979277b812918389d4174d32ddd5c1d
Scott Williams <tocsoft@gmail.com>
2012-03-20 09:32:56 +0000

    ContainsAny

--- Common.Helpers/EnumberableExtentions.cs
+++ Common.Helpers/EnumberableExtentions.cs
@@ -8,12 +8,19 @@ namespace Common.Helpers
     public static class EnumberableExtentions
     {
         public static IEnumerable<T> Do<T>(this IEnumerable<T> lst, Action<T> action) {
-            
             foreach (var itm in lst) {
                 action(itm);
             }
 
             return lst;
+        }
+
+        public static bool ContainsAny<T>(this IEnumerable<T> lst, IEnumerable<T> toFind)
+        {
+            if (lst == null)
+                return false;
+
+            return lst.Where(x => toFind.Contains(x)).Any();
         }
     }
 }
