14cbcaf358dad2917cb1f6e61cd3dd2eaa3f8dad
Scott Williams <tocsoft@gmail.com>
2012-03-27 15:43:23 +0100

    render control to string ++inital umbraco proj

---
+++ Common.Umbraco/DynamicContextExtentions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Common.Helpers;
+using umbraco.cms.businesslogic.macro;
+using umbraco.MacroEngines;
+
+namespace Common.Umbraco
+{
+    public static class DynamicNodeContextExtentions
+    {
+        public static IHtmlString RenderMacro(this DynamicNodeContext ctx, string alias, params object[] properties)
+        {
+            var macro = umbraco.macro.GetMacro(alias);
+            var macroModel = macro.Model;
+
+            if (properties != null)
+            {
+                var dic = properties.ToDictionary();
+
+                foreach (var d in dic)
+                {
+                    macroModel.Properties.Add(new MacroPropertyModel(d.Key, d.Value.ToString()));
+                }
+            }
+
+            var control = macro.renderMacro(macroModel, new System.Collections.Hashtable(), ctx.Node.Id);
+
+            return new HtmlString("");//control.RenderControlToString());
+        }
+    }
+}
