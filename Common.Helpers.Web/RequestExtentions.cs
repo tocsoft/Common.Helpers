8e3ecca07c8bf4e819ac67e9125ce39e3ef65328
Scott Williams <tocsoft@gmail.com>
2012-04-19 15:18:01 +0100

    Request extentions for getting postback string values out or value types

---
+++ Common.Helpers.Web/RequestExtentions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Common.Helpers.Web
+{
+    public static class RequestExtentions
+    {
+        public static TValue GetAs<TValue>(this HttpRequestBase request, string key)
+        {
+            return request[key].As<TValue>();
+        }
+    }
+}
