26f6191849cbafe254eb7301cff0c273e244291e
Scott Williams <tocsoft@gmail.com>
2012-05-21 09:03:03 +0100

    added project deps into nuspec files plus added xsd

--- buildAll.bat
+++ buildAll.bat
@@ -9,7 +9,7 @@ mkdir build
 .nuget\nuget.exe pack -Build Tocsoft.Common.Helpers.Web\Tocsoft.Common.Helpers.Web.csproj -Prop Configuration=Release -OutputDirectory build
 .nuget\nuget.exe pack -Build Tocsoft.Common.Helpers.Json\Tocsoft.Common.Helpers.Json.csproj -Prop Configuration=Release -OutputDirectory build
 .nuget\nuget.exe pack -Build Tocsoft.Common.Database\Tocsoft.Common.Database.csproj -Prop Configuration=Release -OutputDirectory build
-.nuget\nuget.exe pack -Build Tocsoft.Common.Database.SqlCE\Tocsoft.Common.SqlCeDatabase.csproj -OutputDirectory build
+rem .nuget\nuget.exe pack -Build Tocsoft.Common.Database.SqlCE\Tocsoft.Common.SqlCeDatabase.csproj -OutputDirectory build
 .nuget\nuget.exe pack -Build Tocsoft.Common.Umbraco\Tocsoft.Common.Umbraco.csproj -Prop Configuration=Release -OutputDirectory build
 .nuget\nuget.exe pack -Build Tocsoft.Common.Dates\Tocsoft.Common.Dates.csproj -Prop Configuration=Release -OutputDirectory build
 
