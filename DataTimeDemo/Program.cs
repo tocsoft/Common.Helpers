22e6c72927c01cdc7673d67fffc6485b7f939921
Scott Williams <tocsoft@gmail.com>
2013-12-14 20:08:58 +0000

    Pivot Data table extension

--- DataTimeDemo/Program.cs
+++ DataTimeDemo/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using Tocsoft.Common.Dates;
 using Tocsoft.Common.Dates.PatternRules;
+using Tocsoft.Common.Helpers;
 
 namespace DataTimeDemo
 {
@@ -11,23 +13,114 @@ namespace DataTimeDemo
     {
         private static void Main(string[] args)
         {
-            var rptDate = new RepeatingDate();
+            //var rptDate = new RepeatingDate();
 
-            rptDate.Include.Add(new Yearly()
-            {
-                Start = new DateTime(2012, 1, 5)
-            });
-            rptDate.Include.Add(new Yearly()
-            {
-                Start = new DateTime(2012, 1, 5)
-            });
+            //rptDate.Include.Add(new Yearly()
+            //{
+            //    Start = new DateTime(2012, 1, 5)
+            //});
+            //rptDate.Include.Add(new Yearly()
+            //{
+            //    Start = new DateTime(2012, 1, 5)
+            //});
 
-            var dates = rptDate.DatesAny(new DateTime(2012, 1, 1), new DateTime(2016, 1, 1));
-            foreach (var d in dates)
-                Console.WriteLine("{0:dd MMM yyyy, dddd}",d);
-            Console.WriteLine("----------");
-            Console.WriteLine("Total : {0}", dates.Count());
+            //var dates = rptDate.DatesAny(new DateTime(2012, 1, 1), new DateTime(2016, 1, 1));
+            //foreach (var d in dates)
+            //    Console.WriteLine("{0:dd MMM yyyy, dddd}",d);
+            //Console.WriteLine("----------");
+            //Console.WriteLine("Total : {0}", dates.Count());
+            //Console.ReadKey();
+
+
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("EmployeeID", Type.GetType("System.String"));
+            dt.Columns.Add("OrderID", Type.GetType("System.Int32"));
+            dt.Columns.Add("Amount", Type.GetType("System.Decimal"));
+            dt.Columns.Add("Cost", Type.GetType("System.Decimal"));
+            dt.Columns.Add("Date", Type.GetType("System.String"));
+            dt.Rows.Add(new object[] { "Sam", 1, 25.00, 13.00, "01/10/2007" });
+            dt.Rows.Add(new object[] { "Sam", 2, 512.00, 1.00, "02/10/2007" });
+            dt.Rows.Add(new object[] { "Sam", 3, 512.00, 1.00, "03/10/2007" });
+            dt.Rows.Add(new object[] { "Tom", 4, 50.00, 1.00, "04/10/2007" });
+            dt.Rows.Add(new object[] { "Tom", 5, 3.00, 7.00, "03/10/2007" });
+            dt.Rows.Add(new object[] { "Tom", 6, 78.75, 12.00, "02/10/2007" });
+            dt.Rows.Add(new object[] { "Sue", 7, 11.00, 7.00, "01/10/2007" });
+            dt.Rows.Add(new object[] { "Sue", 8, 2.50, 66.20, "02/10/2007" });
+            dt.Rows.Add(new object[] { "Sue", 9, 2.50, 22.00, "03/10/2007" });
+            dt.Rows.Add(new object[] { "Jack", 10, 6.00, 23.00, "02/10/2007" });
+            dt.Rows.Add(new object[] { "Jack", 11, 117.00, 199.00, "04/10/2007" });
+            dt.Rows.Add(new object[] { "Jack", 12, 13.00, 2.60, "01/10/2007" });
+            dt.Rows.Add(new object[] { "Jack", 13, 11.40, 99.80, "03/10/2007" });
+            dt.Rows.Add(new object[] { "Phill", 14, 37.00, 2.10, "02/10/2007" });
+            dt.Rows.Add(new object[] { "Phill", 15, 65.20, 99.30, "04/10/2007" });
+            dt.Rows.Add(new object[] { "Phill", 16, 34.10, 27.00, "02/10/2007" });
+            dt.Rows.Add(new object[] { "Phill", 17, 17.00, 959.00, "04/10/2007" });
+
+
+
+            var output = dt.Pivot(new[]{"Date"}, new[] {"EmployeeID", "Cost"}, d => d.Select(r => r.Field<decimal>("Amount")).Sum());
+
+
+            //render data table to screen
+            PrintTable(dt);
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            PrintTable(output);
+
             Console.ReadKey();
+        }
+
+
+
+
+        static int tableWidth = 77;
+        static void PrintTable(DataTable table) {
+            PrintLine();
+            PrintRow(table.Columns.OfType<DataColumn>().Select(x => x.ColumnName).ToArray());
+
+            PrintLine();
+            foreach (var row in table.Rows.OfType<DataRow>()) {
+
+                PrintRow(row.ItemArray);
+                PrintLine();
+            }
+        }
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        static void PrintRow(params object[] columns)
+        {
+            if (columns.Length > 0)
+            {
+                int width = (tableWidth - columns.Length) / columns.Length;
+                string row = "|";
+
+                foreach (var column in columns)
+                {
+                    row += AlignCentre((column ?? "").ToString(), width) + "|";
+                }
+
+                Console.WriteLine(row);
+            }
+        }
+
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
         }
     }
 }
---
+++ Tocsoft.Common.Helpers/DataTableExtensions.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Tocsoft.Common.Helpers
+{
+    public static class DataTableExtensions
+    {
+
+        const string ColumnNameGroupSeperator = " ~ ";
+
+        private class RowFilter {
+            public RowFilter(RowFilter parent, string col, object val): this(col, val){
+
+                Parent = parent;
+            }
+            public RowFilter(string col, object val) {
+                ColumnName = col;
+                ColumnValue = val;
+            }
+            public string ColumnName { get; set; }
+            public object ColumnValue { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                var filter = obj as RowFilter;
+                if (filter == null)
+                    return false;
+
+                return filter.ColumnName == this.ColumnName
+                    &&
+                    this.ColumnValue.Equals(filter.ColumnValue)
+                    &&
+                    this.Parent == filter.Parent
+                    ;
+            }
+            public override int GetHashCode()
+            {
+                return this.ColumnName.GetHashCode() + this.ColumnValue.GetHashCode() +
+                    (Parent == null ? 0 : Parent.GetHashCode());
+            }
+
+            public static bool operator ==(RowFilter a, RowFilter b)
+            {
+                // If both are null, or both are same instance, return true.
+                if (System.Object.ReferenceEquals(a, b))
+                {
+                    return true;
+                }
+
+                // If one is null, but not both, return false.
+                if (((object)a == null) || ((object)b == null))
+                {
+                    return false;
+                }
+
+                // Return true if the fields match:
+                return a.Equals(b);
+            }
+
+            public static bool operator !=(RowFilter a, RowFilter b)
+            {
+                return !(a == b);
+            }
+            public RowFilter Parent {get;set;}
+
+            public string PivotedName {
+                get { 
+
+                    if (Parent != null)
+                        return Parent.PivotedName + ColumnNameGroupSeperator + ColumnValue;
+
+                    return ColumnValue.ToString();
+                }
+            }
+
+            public bool IsMatch(DataRow row) {
+
+                return row[ColumnName].Equals(ColumnValue) && (Parent == null || Parent.IsMatch(row));
+            }
+            public IEnumerable<object> RowValues() {
+                if (Parent != null) {
+                    foreach (var v in Parent.RowValues())
+                    { 
+                        yield return v;
+                    }
+                }
+                yield return ColumnValue;
+            }
+        }
+       
+
+        public static DataTable Pivot(this DataTable source, string[] rowSources, string[] columnSources,
+            Func<IEnumerable<DataRow>, object> valueCalculation)
+        {
+            var output = new DataTable(source.TableName, source.Namespace);
+
+            //grouping columns
+            
+            var colFilters = columnSources.Skip(1)
+                .Aggregate(
+                    source.Rows.OfType<DataRow>()
+                    .Select(x => new RowFilter(columnSources.First(), x[columnSources.First()]))
+                    .Distinct()
+                    .ToList()
+                , 
+                    (filters, src)=> filters.SelectMany(filter=>
+                        source.Rows.OfType<DataRow>()
+                        .Where(row => filter.IsMatch(row))
+                        .Select(x => new RowFilter(filter, src, x[src]))
+                        .Distinct()
+                        .ToList()
+                    ).Distinct().ToList()
+                ).Distinct().ToList();
+
+
+            var rowFilters = rowSources.Skip(1)
+                .Aggregate(
+                    source.Rows.OfType<DataRow>().Select(x => new RowFilter( rowSources.First(), x[ rowSources.First()]))
+                    .Distinct()
+                    .ToList()
+                , 
+                    (filters, src)=> filters.SelectMany(filter=>
+                        source.Rows.OfType<DataRow>()
+                        .Where(row => filter.IsMatch(row))
+                        .Select(x => new RowFilter(filter, src, x[src]))
+                        .Distinct()
+                        .ToList()
+                    ).Distinct().ToList()
+
+                ).Distinct().ToList();
+
+
+            
+            //name each source in order of depth
+            foreach (var rowSource in rowSources){
+                output.Columns.Add(rowSource);
+            }
+            var ary = colFilters.ToArray();
+            
+            var r = ary[2] == ary[1];
+
+            foreach (var col in colFilters)
+            {
+                output.Columns.Add(col.PivotedName);
+            }
+
+            foreach (var filter in rowFilters)
+            {
+                List<object> rowVals = new List<object>();
+                
+                rowVals.AddRange(filter.RowValues());
+                var rows = source.Rows.OfType<DataRow>()
+                    .Where(x => filter.IsMatch(x))
+                    .ToList();
+                //for each row we now look at each column
+                foreach (var col in colFilters)
+                {
+                    //we now need all the row where the 
+
+                    var matchRows = rows.Where(x => col.IsMatch(x)).ToList();
+                    rowVals.Add(valueCalculation(matchRows));
+
+                }
+
+                output.Rows.Add(rowVals.ToArray());
+            }
+
+            return output;
+        }
+
+
+        public static DataTable Pivot<T>(this DataTable source, string rowSource, string columnSource, string valueSource,
+            Func<IEnumerable<T>, object> valueCalculation)
+        {
+            var output = new DataTable(source.TableName, source.Namespace);
+
+
+            return output;
+        }
+
+        public static DataTable Pivot<T>(this DataTable source, string rowSource, string columnSource, string valueSource)
+        {
+            return source.Pivot<T>(rowSource, columnSource, valueSource, x => x.Count());
+        }
+
+    }
+}
